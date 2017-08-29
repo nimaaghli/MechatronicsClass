4e16862418e3a46de5fdf385d5e88d9102a4078a
Nima <aghli.nima@mail.com>
2017-08-29 17:06:46 -0400

    timer working

---
+++ proj1/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.d
@@ -0,0 +1,86 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /home/nima/mechatronics/proj1/Inc/stm32l4xx_hal_conf.h \
+ /home/nima/mechatronics/proj1/Inc/main.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cm4.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ /home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+/home/nima/mechatronics/proj1/Inc/stm32l4xx_hal_conf.h:
+
+/home/nima/mechatronics/proj1/Inc/main.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/nima/mechatronics/proj1/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
+
+/home/nima/mechatronics/proj1/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
